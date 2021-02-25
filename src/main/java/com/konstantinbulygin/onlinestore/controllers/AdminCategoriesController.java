f6e25dc3e1a01a4fb961e0195e10e387e4d9091d
kostyaFrom <korustlt@gmail.com>
2021-02-25 21:51:27 +0400

    implement GET edit functionality for category

--- src/main/java/com/konstantinbulygin/onlinestore/controllers/AdminCategoriesController.java
+++ src/main/java/com/konstantinbulygin/onlinestore/controllers/AdminCategoriesController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -54,10 +55,21 @@ public class AdminCategoriesController {
         if (categoryExists != null) {
             redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
             redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("categoryInfo", category);
         } else {
             category.setSlug(slug);
             categoryRepository.save(category);
         }
         return "redirect:/admin/categories/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Category category = categoryRepository.getOne(id);
+
+        model.addAttribute("category", category);
+
+        return "admin/categories/edit";
     }
 }
