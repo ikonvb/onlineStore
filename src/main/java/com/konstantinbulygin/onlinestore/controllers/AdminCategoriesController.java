30310c3d497ae6a959df3250a9caec318ab94f08
kostyaFrom <korustlt@gmail.com>
2021-02-25 21:01:36 +0400

    implement adding functionality for category

--- src/main/java/com/konstantinbulygin/onlinestore/controllers/AdminCategoriesController.java
+++ src/main/java/com/konstantinbulygin/onlinestore/controllers/AdminCategoriesController.java
@@ -2,11 +2,16 @@ package com.konstantinbulygin.onlinestore.controllers;
 
 import com.konstantinbulygin.onlinestore.model.CategoryRepository;
 import com.konstantinbulygin.onlinestore.model.data.Category;
+import com.konstantinbulygin.onlinestore.model.data.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -30,5 +35,29 @@ public class AdminCategoriesController {
     public String add(Category category) {
 
         return "admin/categories/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/categories/add";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Category added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = category.getName().toLowerCase().replace(" ", "-");
+
+        Category categoryExists = categoryRepository.findByName(category.getName());
+
+        if (categoryExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+        } else {
+            category.setSlug(slug);
+            categoryRepository.save(category);
+        }
+        return "redirect:/admin/categories/add";
     }
 }
