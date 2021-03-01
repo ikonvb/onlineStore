0f4bf5e5693e29ac568796826327e1871131756d
kostyaFrom <korustlt@gmail.com>
2021-03-01 21:00:17 +0400

    implemented pagination for product

--- src/main/java/com/konstantinbulygin/onlinestore/controllers/AdminProductsController.java
+++ src/main/java/com/konstantinbulygin/onlinestore/controllers/AdminProductsController.java
@@ -3,15 +3,14 @@ package com.konstantinbulygin.onlinestore.controllers;
 import com.konstantinbulygin.onlinestore.model.CategoryRepository;
 import com.konstantinbulygin.onlinestore.model.ProductRepository;
 import com.konstantinbulygin.onlinestore.model.data.Category;
-import com.konstantinbulygin.onlinestore.model.data.Page;
 import com.konstantinbulygin.onlinestore.model.data.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -36,16 +35,31 @@ public class AdminProductsController {
     }
 
     @GetMapping
-    public String index(Model model) {
-        List<Product> products = productRepository.findAll();
+    public String index(Model model, @RequestParam(value = "page", required = false) Integer p) {
+
+        int perPage = 6;
+        int page = (p != null) ? p : 0;
+
+        Pageable pageable = PageRequest.of(page, perPage);
+        Page<Product> products = productRepository.findAll(pageable);
         List<Category> categories = categoryRepository.findAll();
 
         HashMap<Integer, String> categoriesMap = new HashMap<>();
+
         for (Category category : categories) {
             categoriesMap.put(category.getId(), category.getName());
         }
+
+        long count = productRepository.count();
+        double pageCounter = Math.ceil((double) count / (double) perPage);
+
         model.addAttribute("categoriesMap", categoriesMap);
         model.addAttribute("products", products);
+        model.addAttribute("pageCount", (int) pageCounter);
+        model.addAttribute("perPage", perPage);
+        model.addAttribute("count", count);
+        model.addAttribute("page", page);
+
         return "admin/products/index";
     }
 
@@ -129,7 +143,7 @@ public class AdminProductsController {
     }
 
     @PostMapping("/edit")
-    public String edit(@Valid Product product, BindingResult bindingResult, MultipartFile file, RedirectAttributes redirectAttributes, Model model) {
+    public String edit(@Valid Product product, BindingResult bindingResult, MultipartFile file, RedirectAttributes redirectAttributes, Model model) throws IOException {
 
         Product currentProduct = productRepository.getOne(product.getId());
         List<Category> categories = categoryRepository.findAll();
@@ -141,27 +155,17 @@ public class AdminProductsController {
         }
 
         boolean isFileOk = false;
-        byte[] fileBytes = null;
-        String fileName = null;
-        Path path = null;
+        byte[] fileBytes = fileBytes = file.getBytes();
+        String fileName = fileName = file.getOriginalFilename();
+        Path path = Paths.get("src\\main\\resources\\static\\media\\" + fileName);
 
 
-        try {
-            fileBytes = file.getBytes();
-            fileName = file.getOriginalFilename();
-            path = Paths.get("src\\main\\resources\\static\\media\\" + fileName);
-
-            assert fileName != null;
-            if (!file.isEmpty()) {
-                if (fileName.endsWith("jpg") || fileName.endsWith("png")) {
-                    isFileOk = true;
-                }
-            } else {
+        if (!file.isEmpty()) {
+            if (fileName.endsWith("jpg") || fileName.endsWith("png")) {
                 isFileOk = true;
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            isFileOk = true;
         }
 
         redirectAttributes.addFlashAttribute("message", "Product edited");
@@ -183,20 +187,16 @@ public class AdminProductsController {
             product.setSlug(slug);
 
             if (!file.isEmpty()) {
-                try {
-                    Path path1 = Paths.get("src\\main\\resources\\static\\media\\" + currentProduct.getImage());
-                    Files.delete(path1);
-                    product.setImage(fileName);
-                    Files.write(path, fileBytes);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                Path path1 = Paths.get("src\\main\\resources\\static\\media\\" + currentProduct.getImage());
+                Files.delete(path1);
+                product.setImage(fileName);
+                Files.write(path, fileBytes);
             } else {
                 product.setImage(currentProduct.getImage());
             }
             productRepository.save(product);
         }
-        return "redirect:/admin/products/edit";
+        return "redirect:/admin/products/edit/" + product.getId();
     }
 
     @GetMapping("/delete/{id}")
--- src/main/java/com/konstantinbulygin/onlinestore/model/ProductRepository.java
+++ src/main/java/com/konstantinbulygin/onlinestore/model/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.konstantinbulygin.onlinestore.model;
 
 import com.konstantinbulygin.onlinestore.model.data.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
@@ -8,4 +10,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findBySlug(String slug);
 
     Product findBySlugAndIdNot(String slug, int id);
+
+    Page<Product> findAll(Pageable pageable);
 }
