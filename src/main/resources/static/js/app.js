2db2b6c651ae5cdd4587919d964abc2f3b8919b4
kostyaFrom <korustlt@gmail.com>
2021-03-01 21:09:21 +0400

    added ck-editor

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,6 +3,20 @@ $(function () {
     $("a.confirmDeletion").click(function () {
         if(!confirm("Confirm deletion")) return false;
     });
+
+    if($("#content").length) {
+        ClassicEditor.create(document.querySelector("#content"))
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    if($("#descrip").length) {
+        ClassicEditor.create(document.querySelector("#descrip"))
+            .catch(err => {
+                console.log(err);
+            });
+    }
     
 });
 
