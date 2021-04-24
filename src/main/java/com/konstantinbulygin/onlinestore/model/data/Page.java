aaa50096e22687e90404c0e3246caee4ef99808b
kostyaFrom <korustlt@gmail.com>
2021-04-24 15:44:00 +0400

    changed max length of content to 500

--- src/main/java/com/konstantinbulygin/onlinestore/model/data/Page.java
+++ src/main/java/com/konstantinbulygin/onlinestore/model/data/Page.java
@@ -20,7 +20,7 @@ public class Page {
 
     private String slug;
 
-    @Size(min = 5, message = "Content must be at least 5 characters long")
+    @Size(min = 5, max = 500 ,message = "Content must be at least 5 characters long")
     private String content;
 
 }
