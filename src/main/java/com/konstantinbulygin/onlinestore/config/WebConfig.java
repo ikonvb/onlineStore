42c9a970d8474150606956e92bc7905d8dc83891
kostyaFrom <korustlt@gmail.com>
2021-04-24 15:33:52 +0400

    commented WebConfig

--- src/main/java/com/konstantinbulygin/onlinestore/config/WebConfig.java
+++ src/main/java/com/konstantinbulygin/onlinestore/config/WebConfig.java
@@ -1,17 +1,11 @@
-package com.konstantinbulygin.onlinestore.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry
-//                .addResourceHandler("/media/**")
-//                .addResourceLocations("file:/C:/Users/kostyan/IdeaProjects/onlinestore/src/main/resources/static/media/");
-//    }
-}
+//package com.konstantinbulygin.onlinestore.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//}
